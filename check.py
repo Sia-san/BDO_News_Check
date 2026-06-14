a68b7d705dd7e9ba5bb1224e715860e650d7ed48
Sia-san <pon.netzach@gmail.com>
2026-06-15 06:09:59 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,48 @@
-print("test")
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+URL = "https://www.jp.playblackdesert.com/ja-JP/News/Notice?boardType=2"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+html = requests.get(URL, headers=headers, timeout=30).text
+
+soup = BeautifulSoup(html, "html.parser")
+
+article = soup.select_one("ul.thumb_nail_list li a")
+
+if article is None:
+    raise Exception("記事取得失敗")
+
+board_no = article.get("data-boardno")
+link = article.get("href")
+
+current = {
+    "board_no": board_no,
+    "link": link
+}
+
+STATE_FILE = "state.json"
+
+if os.path.exists(STATE_FILE):
+    with open(STATE_FILE, "r", encoding="utf-8") as f:
+        old = json.load(f)
+else:
+    old = {}
+
+if old.get("board_no") != board_no:
+
+    requests.post(
+        os.environ["DISCORD_WEBHOOK"],
+        json={
+            "content":
+            f"📢 黒い砂漠公式お知らせ更新\n{link}"
+        }
+    )
+
+    with open(STATE_FILE, "w", encoding="utf-8") as f:
+        json.dump(current, f, ensure_ascii=False)
